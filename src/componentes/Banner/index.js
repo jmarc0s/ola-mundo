8b36174305f368c51a4e38e39fef54434b5ca133
jmarc0s <joaomarcoscacula88@gmail.com>
2023-10-13 19:04:04 -0300

    desenvolvendo banner

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,32 @@
+import styles from './Banner.module.css'
+import circuloColorido from 'assets/circulo_colorido.png'
+import minhaFoto from 'assets/minha_foto.png'
+
+export default function Banner(){
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentcao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal!, Eu sou Antonio Evaldo
+                </p>
+            </div >
+
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+
+                <img
+                className={styles.minhaFoto}
+                src={minhaFoto}
+                alt="Foto do Antonio Evaldo"/>
+            </div>
+        </div>
+    )
+}
